5217b44c89552261e155381ecc4cea9396d105b0
JokkeIver <jc.ivers1@gmail.com>
2025-04-23 10:53:36 +0200

    [ADDED] comments.lua
    This is used to make nice comments inside your code. Together with the
    plugin i have added custom rebinds in Neovim so i easily can make a text
    into a nice comment

---
+++ .config/nvim/lua/joakim/lazy/comments.lua
@@ -0,0 +1,2 @@
+return { "LudoPinelli/comment-box.nvim" }
+
--- .config/nvim/lua/joakim/remap.lua
+++ .config/nvim/lua/joakim/remap.lua
@@ -3,5 +3,17 @@ vim.keymap.set('n', "<leader>pv", vim.cmd.Ex)
 -- Map double <Esc> to remove search highlight
 vim.api.nvim_set_keymap('n', '<Esc><Esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
 
--- Open Neotree
+--Comments.lua──────────────────────────────────────────────────────────────────────
+-- Titles
+vim.keymap.set({ "n", "v" }, "<Leader>cb", "<Cmd>CBccbox<CR>", {  noremap = true, silent = true })
+-- Named parts
+vim.keymap.set({ "n", "v" }, "<Leader>ct", "<Cmd>CBllline<CR>", {  noremap = true, silent = true })
+-- Simple line
+vim.keymap.set("n", "<Leader>cl", "<Cmd>CBline<CR>", {  noremap = true, silent = true })
+-- vim.keymap.set("i", "<M-l>", "<Cmd>CBline<CR>", {  noremap = true, silent = true }) -- To use in Insert Mode
+-- Marked comments
+vim.keymap.set({ "n", "v" }, "<Leader>cm", "<Cmd>CBllbox14<CR>", {  noremap = true, silent = true })
+-- Removing a box is simple enough with the command (CBd), but if you
+-- use it a lot:
+vim.keymap.set({ "n", "v" }, "<Leader>cd", "<Cmd>CBd<CR>", {  noremap = true, silent = true })
 
