c6f8f01582cb9e6ca07ade308b482b48c0767b1e
JokkeIver <jc.ivers1@gmail.com>
2025-09-02 13:09:56 +0200

    [ADDED] Css to mason-lsp

--- .config/nvim/lua/joakim/lazy/lsp-config.lua
+++ .config/nvim/lua/joakim/lazy/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "svelte", "clangd", "texlab", "jdtls", "html", }
+        ensure_installed = { "lua_ls", "svelte", "clangd", "texlab", "jdtls", "html", "cssls"}
       })
     end
   },
@@ -25,6 +25,7 @@ return {
       lspconfig.clangd.setup({})
       lspconfig.texlab.setup({})
       lspconfig.jdtls.setup({})
+      lspconfig.cssls.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
