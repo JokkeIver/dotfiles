dcbaaa6d93605e65796114823794345ed1aaa794
JokkeIver <jc.ivers1@gmail.com>
2025-09-02 13:58:18 +0200

    [UPDATED] Fixed how telescope talks with todo-comments

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,19 +4,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-local actions = require("telescope.actions")
-local open_with_trouble = require("trouble.sources.telescope").open
+-- Find todo-comments in file
+vim.keymap.set('n', '<leader>ft', "<cmd>TodoTelescope keywords=TODO,FIX,NOTE<cr>", { desc = 'Find todos using telescope'})
 
--- Use this to add more results without clearing the trouble list
-local add_to_trouble = require("trouble.sources.telescope").add
-
-local telescope = require("telescope")
-
-telescope.setup({
-  defaults = {
-    mappings = {
-      i = { ["<c-t>"] = open_with_trouble },
-      n = { ["<c-t>"] = open_with_trouble },
-    },
-  },
-})
