994fa34190712d528636c354acaa756047148c64
JokkeIver <jc.ivers1@gmail.com>
2025-10-29 16:06:55 +0100

    [LSP ADD] Added golang to the LSP setup

--- .config/nvim/lua/joakim/lazy/lsp-config.lua
+++ .config/nvim/lua/joakim/lazy/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "svelte", "clangd", "texlab", "jdtls", "html", "cssls", "ts_ls"}
+        ensure_installed = { "lua_ls", "svelte", "clangd", "texlab", "jdtls", "html", "cssls", "ts_ls", "gopls"}
       })
     end
   },
@@ -27,6 +27,7 @@ return {
       lspconfig.jdtls.setup({})
       lspconfig.cssls.setup({})
       lspconfig.ts_ls.setup({})
+      lspconfig.gopls.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
