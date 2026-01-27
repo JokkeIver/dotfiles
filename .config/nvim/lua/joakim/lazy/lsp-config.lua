23d76bc60bafb555e3ea9a624831b8fa769c6b4b
JokkeIver <jc.ivers1@gmail.com>
2026-01-27 10:56:49 +0100

    [UDPATE LSP]

--- .config/nvim/lua/joakim/lazy/lsp-config.lua
+++ .config/nvim/lua/joakim/lazy/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "svelte", "clangd", "texlab", "jdtls", "html", "cssls", "ts_ls", "gopls"}
+        ensure_installed = { "lua_ls", "svelte", "clangd", "texlab", "jdtls", "html", "cssls", "ts_ls", "gopls" }
       })
     end
   },
